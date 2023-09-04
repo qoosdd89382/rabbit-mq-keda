1e90130a6ab9aa43bd02a9f04249933e76a7e349
cherry <qoosdd89382@gmail.com>
2023-09-04 14:17:08 +0800

    modify k6 script

--- k6/k6.js
+++ k6/k6.js
@@ -1,13 +1,16 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+const durationSec = 5;
+
 export let options = {
     vus: 10,
-    duration: '5s',
+    duration: durationSec + 's',
 };
 
 export default function () {
-    while (true) {
+    const endTime = new Date().getTime() + (durationSec * 1000); // 5秒后结束
+    while (new Date().getTime() < endTime) {
         http.get('http://localhost:8881/send');
         sleep(0.01);
     }
