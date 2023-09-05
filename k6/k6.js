1a1b909f7c1a6e2ef8c89cf539312830fc80e103
cherry <qoosdd89382@gmail.com>
2023-09-05 13:30:21 +0800

    setting k6 script format

--- k6/k6.js
+++ k6/k6.js
@@ -4,17 +4,17 @@ import { sleep } from 'k6';
 const durationSec = 30;
 
 export let options = {
-      scenarios: {
+    scenarios: {
         contacts: {
-          executor: 'constant-vus',
-    vus: 10,
-    duration: durationSec + 's',
-          gracefulStop: '3s',
+            executor: 'constant-vus',
+            vus: 10,
+            duration: durationSec + 's',
+            gracefulStop: '3s',
         },
-      },
+    },
 };
 
 export default function () {
-        http.get('http://producer/send');
-        sleep(0.01);
+    http.get('http://producer/send');
+    sleep(0.01);
 }
