01275c2fc660ee645f05d1d5b4d5226b7faa43a6
cherry <qoosdd89382@gmail.com>
2023-09-06 14:21:08 +0800

    KinD預設imagePullPolicy是Always,所以把測試img推上docker repo
    或者從頭到尾都用load的不要推,並都用Never或IfNotPresent

--- source/collector/src/main/java/com/cherry/collector/controller/CollectorController.java
+++ source/collector/src/main/java/com/cherry/collector/controller/CollectorController.java
@@ -1,11 +1,15 @@
 package com.cherry.collector.controller;
 
 import com.cherry.collector.constant.RabbitMqConstants;
+import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+@CommonsLog
 @RestController
 @RequestMapping(path = "/collect")
 public class CollectorController {
@@ -13,9 +17,12 @@ public class CollectorController {
     @Autowired
     private RabbitMqConstants rabbitMqConstants;
 
+    AtomicInteger atomicInteger = new AtomicInteger(0);
+
     @GetMapping(path = "/do")
-    public void doSomething() {
-        System.out.println("do something...");
+    public void doSomething() throws InterruptedException {
+        Thread.sleep(50);
+        log.info("[" + atomicInteger.getAndIncrement() + "] do something...");
     }
 
     @GetMapping(path = "/getConfig")
